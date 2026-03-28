669c0f13fc0bb5087ae4d8345a17687cff133606
Richmond Apedo <richmondapedo549@gmail.com>
2026-03-28 23:08:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8457653923-6efce",
+  "appId": "[REDACTED_BY_ANTIGRAVITY_APP_ID]",
+  "apiKey": "[REDACTED_BY_ANTIGRAVITY_API_KEY]",
+  "authDomain": "[REDACTED_BY_ANTIGRAVITY_AUTH_DOMAIN]",
+  "measurementId": "",
+  "messagingSenderId": "[REDACTED_BY_ANTIGRAVITY_SENDER_ID]"
+};
